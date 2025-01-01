a80e95298df004fdc806ec27ae283f0832247cc1
Vignesh <vignesh.sureshbabu@discounttire.com>
2025-01-02 02:06:43 +0530

    removed unwanted files

--- Test/src/sample/Javapractice.java
+++ Test/src/sample/Javapractice.java
@@ -8,7 +8,8 @@ public class Javapractice {
 		
 		  int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		  
-		  for (int i = 0; i < arr.length; i++) { if (arr[i] % 2 == 0) {
+		  for (int i = 0; i < arr.length; i++) { 
+			  if (arr[i] % 2 == 0) {
 		  System.out.println(arr[i]); 
 		  break; }
 		  
